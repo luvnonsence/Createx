c60cc1d4201d9796ee724dc7a686eb64fb4a20a1
luvnonsence <136680329+luvnonsence@users.noreply.github.com>
2023-07-09 19:08:05 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,40 +1,26 @@
 $(function () {
 
+  // Фэнси-бокс
+  Fancybox.bind()
+
+  // MixitUP 
   var mixer = mixitup('.directions__list');
 
+  // Обращение к кнопкам по фильтру
   $('.directions__filter-btn').on('click', function () {
     $('.directions__filter-btn').removeClass('directions__filter-btn--active')
     $(this).addClass('directions__filter-btn--active')
   })
 
+  // Smooth Scroll
+  new SmoothScroll('a[href*="#"]');
+
+  // Slick Slider 1
   $('.team__slider').slick({
     arrows: false,
     slidesToShow: 4,
-    infinite: true,
     draggable: false,
     waitForAnimate: false,
-    responsive:
-      [
-        {
-          breakpoint: 1100,
-          settings: {
-            slidesToShow: 3,
-          },
-        },
-        {
-          breakpoint: 750,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 550,
-          settings: {
-            slidesToShow: 1,
-            draggable: true,
-          },
-        },
-      ]
   })
   $('.team__slider-prev').on('click', function (e) {
     e.preventDefault()
@@ -45,20 +31,12 @@ $(function () {
     $('.team__slider').slick('slickNext')
   })
 
+  // Slick Slider 2
   $('.testimonials__slider').slick({
     arrows: false,
     dots: true,
-    appendDots: $('.testimonials__dots'),
     waitForAnimate: false,
-    // responsive:
-    //   [
-    //     {
-    //       breakpoint: 700,
-    //       settings: {
-
-    //       },
-    //     },
-    //   ]
+    appendDots: $('.testimonials__dots'),
   })
   $('.testimonials__prev').on('click', function (e) {
     e.preventDefault()
@@ -69,6 +47,14 @@ $(function () {
     $('.testimonials__slider').slick('slickNext')
   })
 
+  // Аккордеон 1
+  // $('.program__acc-link').on('click', function (e) {
+  //   e.preventDefault()
+  //   $(this).toggleClass('program__acc-link--active')
+  //   $(this).children('.program__acc-text').slideToggle()
+  // })
+
+  // Аккордеон 2
   $('.program__acc-link').on('click', function (e) {
     e.preventDefault()
     if ($(this).hasClass('program__acc-link--active')) {
@@ -82,27 +68,8 @@ $(function () {
     }
   })
 
-  $(".header__nav-list a, .header__top-btn, .footer__go-top").on("click", function (e) {
-    e.preventDefault()
-    var id = $(this).attr('href'),
-      top = $(id).offset().top
-    $('body,html').animate({ scrollTop: top }, 800)
-  })
 
-  setInterval(() => {
-    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
-      $('.burger').addClass('burger--follow')
-    } else {
-      $('.burger').removeClass('burger--follow')
-    }
-  }, 0)
-  $('.burger, .overlay, .header__top a').on('click', function (e) {
-    e.preventDefault()
-    $('.header__top').toggleClass('header__top--open')
-    $('.overlay').toggleClass('overlay--show')
-  })
 
-  $('.footer__top-title--slide').on('click', function () {
-    $(this).next().slideToggle()
-  })
-})
+
+
+});
